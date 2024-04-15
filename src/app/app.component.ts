b304ace7fe83a54963b1034ddd1869011c7ea858
Devulpally-Sindhu <sindhu09.dev@gmail.com>
2024-04-15 12:44:30 +0530

    likes_dislikes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular_practice';
+  likes:number=0;
+  dislikes:number=0;
+
+  like(){
+    this.likes=this.likes+1
+  }
+
+  dislike(){
+    this.dislikes=this.dislikes+1
+  }
 }
