0c16c0385ed329f1814b505e9618a42640a3cc49
Devulpally-Sindhu <sindhu09.dev@gmail.com>
2024-04-15 12:23:57 +0530

    toggle_button_change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular_practice';
+
+  toggleValue:boolean=false;
+
+  toggle(){
+    this.toggleValue=!this.toggleValue
+  }
 }
