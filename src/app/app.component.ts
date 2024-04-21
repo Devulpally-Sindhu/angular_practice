a726fe09304af7f585a15dcb68561b64e68a066e
Devulpally-Sindhu <sindhu09.dev@gmail.com>
2024-04-21 20:49:30 +0530

    form_with_table_CRUD_Operations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular_practice';
+  students:any[]=[];
+
+  firstname:string="";
+  lastname:string="";
+  age:string="";
+
+  toggle:boolean=false;
+  currentIndex:any;
+
+  addStudent(){
+    let studentDetails={
+      fn:this.firstname,
+      ln:this.lastname,
+      age:this.age
+    }
+    this.firstname='';
+    this.lastname='';
+    this.age='';
+    this.students.push(studentDetails)
+  }
+
+  deleteStudent(i:number){
+    this.students.splice(i,1)
+
+  }
+
+  editStudent(i:number){
+    this.currentIndex=i;
+    this.toggle=true;
+    this.firstname=this.students[i].fn;
+    this.lastname=this.students[i].ln;
+    this.age=this.students[i].age;
+
+  }
+
+  saveStudent(){
+    let student={
+      fn:this.firstname,
+      ln:this.lastname,
+      age:this.age
+    }
+    this.students[this.currentIndex]=student;
+    this.toggle=false;
+  }
 }
