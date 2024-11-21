1151b518a27ef592140dab8588eb9ff12868ee7e
Devulpally-Sindhu <sindhu09.dev@gmail.com>
2024-11-21 18:55:47 +0530

    sdfg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { Amma1Component } from './amma1/amma1.component';
-import { Amma2Component } from './amma2/amma2.component';
-import { Amma3Component } from './amma3/amma3.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    Amma1Component,
-    Amma2Component,
-    Amma3Component,
-  
+    
   ],
   imports: [
     BrowserModule,
